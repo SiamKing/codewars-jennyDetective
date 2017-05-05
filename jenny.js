227460cbe308705c48302334824099d3ce7c8c15
SiamKing <thesiameseelectric@gmail.com>
2017-05-05 16:19:57 -0700

    fixed

--- jenny.js
+++ jenny.js
@@ -5,7 +5,7 @@ function missingWord(nums, str) {
   nums = nums.sort((a, b) => a - b);
 
   for (i = 0; i < nums.length; i++) {
-    if (str[nums[i]] !== 'undefined') {
+    if (str[nums[i]] !== undefined) {
       s += str[nums[i]].toLowerCase();
     } else {
       return "No mission today"
